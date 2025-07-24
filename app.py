db86defec4fea69beb6f58fdf775219b63ec188a
Krishna Yadav <yadavkrish2709@gmail.com>
2025-07-24 17:51:38 +0530

    loaded the model

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost as xgp
+import xgboost as xgb
 
 #page setup
 
@@ -10,3 +10,16 @@ st.set_page_config(
   page_icon="🔒",
   layout="wide"
 )
+# Load model
+@st.cache_resource
+def load_model():
+  try:
+    model=xgb.XGBClassifier()
+    model.load_model("xgb_fraud_model.json")
+    return model
+  except FileNotFoundError:
+    st.error("Model File 'xgp_fraud_model.json' not found!")
+    return None
+  except Exception as e:
+    st.error(f"Error loading model:{str(e)}")
+    return None

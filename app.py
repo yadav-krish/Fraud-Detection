7462d5bfe33f0276323fca75207dc9f6c0f2ac23
Krishna Yadav <yadavkrish2709@gmail.com>
2025-07-24 17:41:38 +0530

    set the page icon and title

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import xgboost as xgp
+
+#page setup
+
+st.set_page_config(
+  page_title="Fraud Detection Sustem",
+  page_icon="🔒",
+  layout="wide"
+)
